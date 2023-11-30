bf31b031136b7b20eec74b31e73e9dd7b2c44d92
Fasken2 <91476287+Fasken2@users.noreply.github.com>
2023-11-30 11:15:33 +0100

    Tested in simulation _ working_needs tuning

--- Viktor/LAB_B/LabB_ControllerOverSimulator_Continuous_Parameters.m
+++ Viktor/LAB_B/LabB_ControllerOverSimulator_Continuous_Parameters.m
@@ -2,7 +2,12 @@ close all;
 clear all;
 clc;
 
-
-K = [ -10.0000  -57.4908 -105.0371  -19.5009 ]
-
+load Linear_Param.mat
+omegan = 5.7;
+zeta = 0.8;
+ideal_poles = [-475;
+                -6*omegan*zeta;
+                omegan*(-zeta+1i*sqrt(1-zeta^2));
+                omegan*(-zeta-1i*sqrt(1-zeta^2))];
+K = acker(A,B(:,1),ideal_poles);
 
