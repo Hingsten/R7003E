29ccd9e3a40ea6530cd8f6ec637a0ed3921eab77
Fasken2 <91476287+Fasken2@users.noreply.github.com>
2023-12-05 14:08:37 +0100

    Fixed plots

--- Viktor/LAB_B/Plot4_8_PID.m
+++ Viktor/LAB_B/Plot4_8_PID.m
@@ -15,9 +15,32 @@ legend("Simulator","Full order","Reduced order")
 print('-depsc2', '-r300', 'LabB_4_8_xw_PID.eps');
 
 figure
-plot(thetab.time(index),thetab.signals.values(index,:))
+plot(thetab.time(index),thetab.signals.values(index,:)*180/pi)
 title("$$\theta_b$$"); xlabel('time'); ylabel('degrees')
 set(gcf, 'Units', 'centimeters'); set(gcf,'Position',afFigurePosition);
 set(gcf, 'PaperPositionMode', 'auto');
 legend("Simulator","Full order","Reduced order")
 print('-depsc2', '-r300', 'LabB_4_8_thetab_PID.eps');
+
+figure
+plot(thetab.time(index),abs(thetab.signals.values(index,1)-thetab.signals.values(index,2))*180/pi)
+hold on
+plot(thetab.time(index),abs(thetab.signals.values(index,1)-thetab.signals.values(index,3))*180/pi)
+hold off
+title("$$\theta_b$$"); xlabel('time'); ylabel('degrees')
+set(gcf, 'Units', 'centimeters'); set(gcf,'Position',afFigurePosition);
+set(gcf, 'PaperPositionMode', 'auto');
+legend("Full order","Reduced order")
+print('-depsc2', '-r300', 'LabB_4_8_thetab_ABSDIFF_PID.eps');
+
+
+figure
+plot(xw.time(index),abs(xw.signals.values(index,1)-xw.signals.values(index,2)))
+hold on
+plot(xw.time(index),abs(xw.signals.values(index,1)-xw.signals.values(index,3)))
+hold off
+title("$$x_w$$"); xlabel('time'); ylabel('meters')
+set(gcf, 'Units', 'centimeters'); set(gcf,'Position',afFigurePosition);
+set(gcf, 'PaperPositionMode', 'auto');
+legend("Full order","Reduced order")
+print('-depsc2', '-r300', 'LabB_4_8_xw_ABSDIFF_PID.eps');
